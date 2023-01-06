e75cd3f4aa38554886751a8957b88f2a5a3b9542
Arunkumar Selvaraj <arun@ioak.io>
2023-01-06 09:16:01 +0530

    Fix for nav bar on home page

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react';
+import { loginPageSubject } from '../../events/LoginPageEvent';
 import './style.scss';
 
 interface Props {
 }
 const Home = (props: Props) => {
+  useEffect(() => {
+    loginPageSubject.next({ state: false });
+    return () => {
+      loginPageSubject.next({ state: true });
+    };
+  }, []);
   return (
     <div className="page-home">
       <div className="app-container smooth-page page-home--title">
